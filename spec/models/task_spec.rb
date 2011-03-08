1de24310ca3ddce5f0e7e2bb9a1499dabc6cc646
David W. Body <davidbody@bigcreek.com>
2011-03-07 20:32:16 -0600

    Added a model spec

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe Task do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "with valid parameters" do
+    before(:each) do
+      @task = Task.new(:name => 'Learn Ruby', :description => 'Read PickAxe')
+    end
+
+    it "should be valid" do
+      @task.should be_valid
+    end
+  end
 end
